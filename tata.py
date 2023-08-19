6143d61f6603232e9ea76d65b0f16a0aa32e230b
circi <circi@gmail.com>
2023-08-19 09:01:11 +0000

    updated tata.py

--- tata.py
+++ tata.py
@@ -1,6 +1,6 @@
 # (in w-notation)
 cube = '01_1_ABCD,MNOP_2_abcd,hfrt,mnop_'
-td_notation_notes = ['UFL', 'ULB', 'UBR', 'URF', # U-face, clockwise from bottom-left (blue in BTRF; ABCD ; 01020304 ;
+td_corners = ['UFL', 'ULB', 'UBR', 'URF', # U-face, clockwise from bottom-left (blue in BTRF; ABCD ; 01020304 ;
                      'FUR', 'FLU', 'FDL', 'FRD', # F-face, ccw from top-right (red in BTRF); EFGH ; 05060708 ;
                      'RDF', 'RFU', 'RUB', 'RBD', # R-face, cw from bottom-left facing R (yellow in BTRF); IJKL ; 09101112 ;
                      'DRB', 'DFR', 'DLF', 'DBL', # D-face, ccw from bottom-right facing D (green in BTRF); MNOP ; 13141516 ;
@@ -18,14 +18,14 @@ td_edges = ['UL', 'UB', 'UR', 'UF',
 
 
 # to be streamlined/removed/renamed at another date due to ambiguity in notation and inconsistency in representation
-_nf_edges_state_solved = 'abcdefghijklmnopqrstuvwx_,'
-_nf_corners_state_solved = 'ABCDEFGHIJKLMNOPQRSTUVWX_,'
+_nf_w_edges_state_solved = 'abcdefghijklmnopqrstuvwx_,'
+_nf_w_corners_state_solved = 'ABCDEFGHIJKLMNOPQRSTUVWX_,'
 
-_nf_edges_state_R = ''
-_nf_corners_state_R = ''
+_nf_w_edges_state_R = ''
+_nf_w_corners_state_R = ''
 
-_nf_edges_state_x = ''
-_nf_corners_state_x = ''
+_nf_w_edges_state_x = ''
+_nf_w_corners_state_x = ''
 
 _nf_modes_dict = {'01' : 'w;wii;where-is-it;where-is-it notation;', # implies compressed notation
                   '02-0' : 'p;p;positional;positional notation;', # likewise for all other modes in the current dictionary
@@ -38,17 +38,41 @@ _nf_modes_uncompressed_dict = {'01-f' : 'w-full', '02-f' : 'p-full'}
 # some moves/states (mostly for reference, usage possible)
 _nf_solved_w_expanded_unannotated = 'ABCDEFGHIJKLMNOPQRSTUVWXabcdefghijklmnopqrstuvwx'
 # positions of one of the stickers of each piece uniquely determines a cube state (c->8 or 7, e->12 or 11)
-_nf_solved_w_compressed_unannotated = 'ABCD,MNOP,abcd,hfrt,mnop'
+_nf_solved_w_compressed_unannotated_bare = 'ABCDMNOPabcdhfrtmnop'
 # w and p notations are the same in the solved state
 _nf_solved_p_expanded_unannotated = _nf_solved_w_expanded_unannotated
-_nf_solved_p_compressed_unannotated = _nf_solved_w_compressed_unannotated
+_nf_solved_p_compressed_unannotated_bare = _nf_solved_w_compressed_unannotated
 
-# note: incomplete; does not account for edges yet
+# note: incomplete; does not account for edges yeta
+compress_notation_debug = False
 def compress_notation(cube, mode='01', human_readable=True):
+    if mode != '01':
+        print('compress_notation: other modes are not yet supported, returning original cube...')
+        return cube
     if human_readable == True:
-        cube_in_compressed_notation = ''.join([[cube[x] for x in [0,1,2,3]].append(',').append([cube[x] for x in [12,13,14,15]])])
+        ## cube_in_compressed_notation = ''.join([[cube[x] for x in [0,1,2,3]].append(',').append([cube[x] for x in [12,13,14,15]])])
+        ## todo: add stripping / reformatting
+        cicnl = []
+        cube_in_compressed_notation_list = cicnl
+        fu_corners = [0,1,2,3]
+        fd_corners = [12,13,14,15]
+        pos_a = _nf_solved_w_expanded_unannotated.indexof('a')
+        pos_h = _nf_solved_w_expanded_unannotated.indexof('h')
+        pos_r =  _nf_solved_w_expanded_unannotated.indexof('r')
+        pos_f =  _nf_solved_w_expanded_unannotated.indexof('m')
+
+        fu_edges = [pos_a, pos_a + 1, pos_a + 2, pos_a + 3]
+        se_edges = [pos_h, pos_h - 2, pos_r, pos_r + 2]
+        fd_edges = [pos_m, pos_m + 1, pos_m + 2, pos_m + 3]
+        if compress_notation_debug == True:
+            print(f"pos_* test:: pos_a: {pos_a}, pos_h: {pos_h}, pos_r: {pos_r}, pos_f: {pos_f}")
+        for piece_type in [fu_corners, fd_corners, fu_edges, fd_edges]: # fu -> U-face, fd -> D-face, etc.
+            cicnl.append([cube[x] for x in piece_type])
+            cicnl.append(',')
+        cube_in_compressed_notation = ''.join(cicnl)
+
     elif human_readable == False:
-        cube_in_compressed_notation = ''.join([cube[x] for x in [0,1,2,3,12,13,14,15]])
+        cube_in_compressed_notation = ''.join([cube[x] for x in [0,1,2,3,12,13,14,15,24,25,26,27,36,37,38,39]])
     return cube_in_compressed_notation
 
 def expand_notation(cube, mode='01'):
@@ -91,7 +115,17 @@ def make_new_cube(mode='01'):
 
 
 def main():
-    pass
+    print('compress_notation test:')
+    print(compress_notation('ABCDEFGHIJKLMNOPQRSTUVWXabcdefghijklmnopqrstuvwx'))
+    print(compress_notation('ABCDEFGHIJKLMNOPQRSTUVWXabcdefghijklmnopqrstuvwx', human_readable=False))
+
+    cube = make_new_cube()
+    cube = moves(cube, ['R'])
+    print(f"cube;R: {cube}")
+    cube = moves(cube, ['x', 'U'])
+    print(f"cube;R: {cube}")
+    print()
+    return
 
 if __name__ == '__main__':
     main()
